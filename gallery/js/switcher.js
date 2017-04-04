3cceb4ccd4c6d65455e022b23d5aaac39fd0ca6a
AnnaMakarova <anko.kagami@gmail.com>
2017-04-04 19:28:42 +0300

    show 1st pic instead of default gif

--- gallery/js/switcher.js
+++ gallery/js/switcher.js
@@ -1,7 +1,9 @@
 'use strict';
-var index = 4,
+var index = 0,
   imgs = ['i/breuer-building.jpg', 'i/guggenheim-museum.jpg', 'i/headquarters.jpg', 'i/IAC.jpg', 'i/new-museum.jpg'];
 const currentImg = document.getElementById('currentPhoto');
+//apply 1st pic instead of gif by default
+currentImg.src = imgs[index];
 //direction - use 1 to switch next, -1 to switch back
 function switchImg(direction) {
   index += direction;
--- slider/js/slider.js
+++ slider/js/slider.js
@@ -1,5 +1,5 @@
 'use strict';
-var index = 4,
+var index = 0,
   imgs = ['i/airmax-jump.png', 'i/airmax-on-foot.png', 'i/airmax-playground.png', 'i/airmax-top-view.png', 'i/airmax.png'];
 function slideNext() {
   index++;
@@ -9,6 +9,8 @@ function slideNext() {
   return imgs[index];
 }
 const sliderImg = document.getElementById('slider');
+//apply 1st pic instead of default gif (to show it on the 0 second)
+sliderImg.src = imgs[index];
 setInterval(function () {
   sliderImg.src = slideNext();
 }, 5000);
