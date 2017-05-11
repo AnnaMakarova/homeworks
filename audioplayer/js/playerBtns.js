86ba4a8d3bf3c74313f640f343b93b396bbb9234
AnnaMakarova <anko.kagami@gmail.com>
2017-05-11 21:34:24 +0300

    правки по заданию 3

--- audioplayer/js/playerBtns.js
+++ audioplayer/js/playerBtns.js
@@ -1,12 +1,12 @@
 'use strict';
 const audios = ['mp3/LA Chill Tour.mp3', 'mp3/LA Fusion Jam.mp3', 'mp3/This is it band.mp3'];
-var cur_song = 0;
+let cur_song = 0;
 const titleEL = document.getElementsByClassName('title')[0];
 const player = document.getElementsByTagName('audio')[0];
 const mediaplayer = document.getElementsByClassName('mediaplayer')[0];
 function setAudio() {
   if (titleEL) {
-    titleEL.title = audios[cur_song].slice(4, audios[cur_song].length - 4);
+    titleEL.title = audios[cur_song].slice(4, -4);
   }
   if (player) {
     player.src = audios[cur_song];
