9cd0a113a1f8ae6ff197f3186afe10c296bcd6ef
AnnaMakarova <anko.kagami@gmail.com>
2017-06-14 18:54:00 +0300

    домашняя работа  «Изменение структуры HTML-документа»

---
+++ html-document-structure/tabs/js/tabs.js
@@ -0,0 +1,30 @@
+'use strict';
+const tabs = document.getElementById('tabs');
+const tabsContent = tabs.getElementsByClassName('tabs-content')[0];
+const tabsContentItems = tabsContent.children;
+const tabsNav = tabs.getElementsByClassName('tabs-nav')[0];
+const navItem = tabsNav.firstElementChild;
+tabsNav.removeChild(navItem);
+for (let item of tabsContentItems) {
+  let newNav = navItem.cloneNode(true);
+  newNav.firstElementChild.textContent = item.dataset.tabTitle;
+  newNav.firstElementChild.classList.add(item.dataset.tabIcon);
+  tabsNav.appendChild(newNav);
+}
+tabsNav.firstElementChild.classList.add('ui-tabs-active');
+function tabsClick(eo) {
+  let curTab = eo.target;
+  for (let navItem of tabsNav.children) {
+    navItem.classList.remove('ui-tabs-active');
+  }
+  curTab.parentNode.classList.add('ui-tabs-active');
+  let curTitle = curTab.textContent;
+  for (let item of tabsContentItems) {
+    if (item.dataset.tabTitle == curTitle) {
+      item.classList.remove('hidden');
+    } else {
+      item.classList.add('hidden');
+    }
+  }
+}
+tabsNav.addEventListener('click', tabsClick);
